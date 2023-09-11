7d88b5009913f2f261e5925e721e70b8197eb708
Michal Malý <madcatxster@devoid-pointer.net>
2023-09-11 20:06:53 +0200

    Avoid excessive redraws when Options change values

--- src/ui/form/components/options.tsx
+++ src/ui/form/components/options.tsx
@@ -12,19 +12,19 @@ import { niceLabel } from '../util';
 import { SOnChange } from '../typedefs';
 import { assert } from '../../../assert';
 import { FormContextInstance } from '../../../context';
+import { _FormContextHandler } from '../../../context/handler';
 import { Choice, Help, OptionsItem, Schema } from '../../../schema';
 import { Path } from '../../../schema/data';
 import { Value } from '../../../schema/value';
 import { CommonValidators } from '../../../schema/validators';
 
-type Option = {
-    key: number,
-    text: string,
-    value: string,
-};
-
 const BorderStyle = { border: '1px solid #ccc' };
 const PadStyle = { padding: '10px' };
+
+function initialState(handler: _FormContextHandler, path: Path) {
+    const value = handler.getValue(path);
+    return { tag: Value.toOption(value), isValid: value.isValid };
+}
 
 function mkOptions(choices: OptionsItem['choices'], dontTransform: boolean) {
     const opts = choices.map((choice, idx) => ({
@@ -37,23 +37,45 @@ function mkOptions(choices: OptionsItem['choices'], dontTransform: boolean) {
     return opts;
 };
 
-const _Selection = React.memo(function MSelection({ id, onChange, options, value, noRightOffset, isDisabled, error }: {
+const _Selection = React.memo(function MSelection({ id, choices, noRightOffset, isDisabled, isRequired, path, handler, dontTransformContent }: {
     id: string,
-    onChange: SOnChange<SDropdownProps>,
-    options: Option[]
-    value: string,
+    choices: Choice[],
     isDisabled: boolean,
+    isRequired: boolean,
+    path: Path,
+    handler: _FormContextHandler,
     noRightOffset?: boolean,
-    error?: boolean,
+    dontTransformContent?: boolean,
 }) {
+    const opts = React.useMemo(() => {
+        return mkOptions(choices, !!dontTransformContent);
+    }, [choices, dontTransformContent]);
+    const [localValue, setLocalValue] = React.useState(initialState(handler, path));
+    const onChange: SOnChange<SDropdownProps> = (_ev, data) => {
+        const tag = data.value as string;
+        const choice = choices.find((c) => c.tag === tag);
+        assert(choice !== undefined || tag === Schema.EmptyChoice, `No choice with tag "${tag}"`);
+
+        const newValue = tag === Schema.EmptyChoice ? Value.emptyOption(!isRequired) : Value.option(tag);
+        handler.set(path, newValue, newValue.isValid === localValue.isValid);
+        setLocalValue({ tag, isValid: newValue.isValid });
+    };
+    React.useEffect(() => {
+        const value = handler.getValue(path);
+        const tag = Value.toOption(value);
+        if (tag !== localValue.tag || value.isValid !== localValue.isValid) {
+            setLocalValue({ tag, isValid: value.isValid });
+        }
+    }, [path]);
+
     return (
         <SDropdown
             className={clsx(!noRightOffset && 'mbdb-right-offset')}
             id={id}
-            value={value}
+            value={localValue.tag}
             onChange={onChange}
-            options={options}
-            error={error}
+            options={opts}
+            error={!localValue.isValid}
             disabled={isDisabled}
             selection
         />
@@ -61,9 +83,12 @@ const _Selection = React.memo(function MSelection({ id, onChange, options, value
 }, (prevProps, nextProps) => {
     return (
         Object.is(prevProps.id, nextProps.id) &&
-        Object.is(prevProps.options, nextProps.options) &&
-        Object.is(prevProps.value, nextProps.value) &&
-        Object.is(prevProps.isDisabled, nextProps.isDisabled)
+        Object.is(prevProps.choices, nextProps.choices) &&
+        Object.is(prevProps.isDisabled, nextProps.isDisabled) &&
+        Object.is(prevProps.isRequired, nextProps.isRequired) &&
+        Object.is(prevProps.path, nextProps.path) &&
+        Object.is(prevProps.noRightOffset, nextProps.noRightOffset) &&
+        Object.is(prevProps.dontTransformContent, nextProps.dontTransformContent)
     );
 });
 
@@ -73,37 +98,27 @@ export type Props = {
     help?: Help,
     path: Path,
     isDisabled: boolean,
+    isRequired: boolean,
     dontTransformContent?: boolean,
     noRightOffset?: boolean,
-    isRequired: boolean,
 }
 
 export function OptionsInput(props: Props) {
     const id = React.useMemo(() => PathId.toId(props.path), [props.path]);
-    const opts = React.useMemo(() => {
-        return mkOptions(props.choices, !!props.dontTransformContent);
-    }, [props.choices]);
     const { handler } = React.useContext(FormContextInstance);
-    const onChange: SOnChange<SDropdownProps> = (_ev, data) => {
-        const tag = data.value as string;
-        const choice = props.choices.find((c) => c.tag === tag);
-        assert(choice !== undefined || tag === Schema.EmptyChoice, `No choice with tag "${tag}"`);
 
-        handler.set(props.path, tag === Schema.EmptyChoice ? Value.emptyOption(!props.isRequired) : Value.option(tag));
-    };
-
-    const v = handler.getValue(props.path);
     return (
         <>
             <ItemLabel label={props.label} help={props.help} markAsRequired={props.isRequired} id={id} />
             <_Selection
                 id={id}
-                options={opts}
-                value={Value.toOption(v)}
-                onChange={onChange}
+                choices={props.choices}
                 isDisabled={props.isDisabled}
+                isRequired={props.isRequired}
+                handler={handler}
+                path={props.path}
                 noRightOffset={props.noRightOffset}
-                error={Value.isEmptyOption(v) && props.isRequired}
+                dontTransformContent={props.dontTransformContent}
             />
         </>
     );
@@ -111,21 +126,11 @@ export function OptionsInput(props: Props) {
 
 export function OptionsWithOtherInput(props: Props) {
     const id = React.useMemo(() => PathId.toId(props.path), [props.path]);
-    const opts = React.useMemo(() => {
-        return mkOptions(props.choices, !!props.dontTransformContent);
-    }, [props.choices]);
     const { handler } = React.useContext(FormContextInstance);
 
     const value = handler.getValue(props.path);
     assert(Value.isOption(value), 'Expected an Option value');
 
-    const onChangeOptions: SOnChange<SDropdownProps> = (_ev, data) => {
-        const tag = data.value as string;
-        const choice = props.choices.find((c) => c.tag === tag);
-        assert(choice !== undefined || tag === Schema.EmptyChoice, `No choice with tag "${tag}"`);
-
-        handler.set(props.path, tag === Schema.EmptyChoice ? Value.emptyOption(!props.isRequired) : Value.option(tag));
-    };
     const onChangeText: SOnChange<SInputProps> = (_ev, data) => {
         const v = data.value as string;
         handler.set(props.path, Value.option(Schema.OtherChoice, v), CommonValidators.isSet(v));
@@ -136,12 +141,13 @@ export function OptionsWithOtherInput(props: Props) {
             <ItemLabel label={props.label} help={props.help} markAsRequired={props.isRequired} id={id} />
             <_Selection
                 id={id}
-                options={opts}
-                value={value.payload.tag}
-                onChange={onChangeOptions}
+                choices={props.choices}
                 isDisabled={props.isDisabled}
+                isRequired={props.isRequired}
+                handler={handler}
+                path={props.path}
                 noRightOffset={props.noRightOffset}
-                error={Value.isEmptyOption(value) && props.isRequired}
+                dontTransformContent={props.dontTransformContent}
             />
             {Schema.isOtherChoice(value)
                 ? (
