6ea1a21c375dd61f217f8b56622d3738e7f6f09d
Michal Malý <madcatxster@devoid-pointer.net>
2023-10-28 23:47:01 +0200

    Use a shared defintion of GeneralParameters instead of inlining it in every model definition

--- src/schema/configuration/common.ts
+++ src/schema/configuration/common.ts
@@ -48,8 +48,9 @@ export const Common = {
         ],
     },
 
-    '*/pH': {
+    '*/ph': {
         dontTransformLabels: true,
+        label: 'pH',
     },
 
     '*/unit': {
--- util/generate_schemas.sh
+++ util/generate_schemas.sh
@@ -1,17 +1,61 @@
 #! /bin/bash
 
+function tsify_file() {
+    echo $(echo "$1" | sed "s/_/-/g")".ts"
+}
+
+function tsify_var() {
+	TSIFIED=""
+	IFS="_" read -ra PARTS <<< "$1"
+	for part in ${PARTS[@]}; do
+		TSIFIED="${TSIFIED}${part^}"
+	done
+
+	echo $TSIFIED
+}
+
+
+function fail() {
+    echo "Failed to convert $1, exiting prematurely..."
+    exit 1
+}
+
 
 INPUT_PATH=../../mbdb-model/models/oarepo/
 OUTPUT_PATH=../src/schema/schemas/
+
+# Imports are not supposed to be used directly as models
+# but they are used (=imported) by the actual models
+# Imports allow us to factor out common parts of models
+IMPORTS=(general_parameters)
+
+for imp in ${IMPORTS[@]}
+do
+    OUTFILE=$(tsify_file "${imp,,}")
+    OUTVAR=$(tsify_var "${imp,,}")
+    echo "Starting to convert $imp import"
+        ./make_ui_defn.py                     \
+        --input  "${INPUT_PATH}${imp}.yaml"   \
+        --output "${OUTPUT_PATH}${OUTFILE}"   \
+        --schema_name "$OUTVAR"               \
+        || fail "$imp"
+	echo "Finished converting imports"
+done
+
 
 MODELS=(MST BLI SPR)
 
 for mod in ${MODELS[@]}
 do
-echo "Starting to convert $mod model"
-    ./make_ui_defn.py                  \
-    --input $INPUT_PATH$mod.yaml       \
-    --output $OUTPUT_PATH"${mod,,}".ts \
-    --schema_name $mod
-echo "Finished"
+    OUTFILE=$(tsify_file "${mod,,}")
+    echo "Starting to convert $mod model"
+        ./make_ui_defn.py                     \
+        --input  "${INPUT_PATH}${mod}.yaml"   \
+        --output "${OUTPUT_PATH}${OUTFILE}"   \
+        --schema_name "$mod"                  \
+        --imported_defis General_parameters   \
+        || fail "$mod"
+echo "Finished converting models"
 done
+
+echo "All done"
