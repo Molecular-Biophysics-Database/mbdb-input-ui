881cad30467990d15abd060120d9f54292636a32
Michal Malý <madcatxster@devoid-pointer.net>
2023-11-23 20:27:52 +0100

    Make sure that we fetch the entire vocabulary

--- src/mbdb/vocabulary.ts
+++ src/mbdb/vocabulary.ts
@@ -15,7 +15,60 @@ export type Vocabulary = VocabularyEntry[];
 
 const Cache = new Map<string, Vocabulary>;
 
-function makeUrl(type: string) {
+async function getTotalHits(type: string) {
+    const url = makeUrl(type);
+    const pending = issueRequest(url);
+
+    const resp = await Net.resolveFetch(pending);
+    if (!resp.ok) {
+        throw new Error(`"Error ${resp.status}" response to vocabulary request for "${type}" vocabulary type.`);
+    }
+
+    const payload = await resp.json();
+    const totalHits = payload['hits']?.['total'];
+    if (typeof totalHits !== 'number' || totalHits < 0) {
+        throw new Error(`Unexpected response to vocabulary request for "${type}" vocabulary type. Invalid number of total hits.`);
+    }
+
+    return totalHits;
+}
+
+async function getVocabularyItems(type: string, totalHits: number) {
+    const url = makeUrl(type, totalHits);
+    const pending = issueRequest(url);
+
+    const resp = await Net.resolveFetch(pending);
+    if (!resp.ok) {
+        throw new Error(`"Error ${resp.status}" response to vocabulary request for "${type}" vocabulary type.`);
+    }
+
+    const payload = await resp.json();
+    const voc = payload['hits']?.['hits'];
+    if (!voc) {
+        throw new Error(`Unexpected response to vocabulary request for "${type}" vocabulary type.`);
+    }
+    if (!isVocabulary(voc)) {
+        throw new Error(`Vocabulary "${type}" does not conform to the expected schema.`)
+    }
+
+    return voc;
+}
+
+function issueRequest(url: string) {
+    return Net.fetchWithTimeout(
+        url,
+        {
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            cache: 'no-cache',
+        },
+        15000
+    );
+}
+
+function makeUrl(type: string, size?: number) {
     if (!Config.get('vocabulariesApiEndpoint')) {
         throw new Error('Cannot make a URL for vocabulary API query because the application is misconfigured.');
     }
@@ -23,7 +76,12 @@ function makeUrl(type: string) {
     // BEWARE, BEWARE, BEWARE:
     // The inconsistency, or rather lack of thereof, is about the hit you in the face.
     // This URL shall *not* end with a trailing slash, otherwise the API request will fail.
-    return `${Config.get('baseUrl')}/api/${Config.get('vocabulariesApiEndpoint')}/${type}`;
+    let url =`${Config.get('baseUrl')}/api/${Config.get('vocabulariesApiEndpoint')}/${type}`;
+    if (size) {
+        url = `${url}?page=1&size=${size}`;
+    }
+
+    return url;
 }
 
 function isVocabulary(obj: any): obj is Vocabulary {
@@ -57,32 +115,12 @@ export const Vocabulary = {
     async get(type: string, forceRefresh = false): Promise<Vocabulary> {
         if (!forceRefresh && Cache.has(type)) return Cache.get(type)!;
 
-        const url = makeUrl(type);
-        const pending = Net.fetchWithTimeout(
-            url,
-            {
-                method: 'GET',
-                headers: {
-                    'Content-Type': 'application/json',
-                },
-                cache: 'no-cache',
-            },
-            15000
-        );
+        // This needs to be done in double pass because the backend
+        // returns only limited number of item unless we explicitly
+        // ask for the number of vocabulary items to return.
 
-        const resp = await Net.resolveFetch(pending);
-        if (!resp.ok) {
-            throw new Error(`"Error ${resp.status}" response to vocabulary request for "${type}" vocabulary type.`);
-        }
-
-        const json = await resp.json();
-        const voc = json['hits']?.['hits'];
-        if (!voc) {
-            throw new Error(`Unexpected response to vocabulary request for "${type}" vocabulary type.`);
-        }
-        if (!isVocabulary(voc)) {
-            throw new Error(`Vocabulary "${type}" does not conform to the expected schema.`)
-        }
+        const totalHits = await getTotalHits(type);
+        const voc = await getVocabularyItems(type, totalHits);
 
         Cache.set(type, voc);
 
