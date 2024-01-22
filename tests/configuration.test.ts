7493dfcbe17b26dad65a8b3741c4f9589ca37b67
Emil Dandanell Agerschou <emildandanell.agerschou@ibt.cas.cz>
2024-01-22 16:40:44 +0100

    removed tests that referred to fields that are no longer present

--- tests/configuration.test.ts
+++ tests/configuration.test.ts
@@ -40,15 +40,4 @@ test('Invalid configuration checks', () => {
     badConfig = deepCopy(config);
     badConfig['non_existent/field'] = {};
     expect(() => Configuration.configure(schema.schema, badConfig)).toThrow('Schema configuration error: No configurees for configuration item "non_existent/field".');
-
-    badConfig = deepCopy(config);
-    badConfig['general_parameters/associated_publications/main/publication_year'] = { defaultValue: 1700 };
-    expect(() => Configuration.configure(schema.schema, badConfig)).toThrow('Schema configuration error: Default value is outside the allowed range "1900 - 1.7976931348623157e+308".');
-
-    badConfig['general_parameters/associated_publications/main/publication_year'].defaultValue = 'text_is_not_okay_here';
-    expect(() => Configuration.configure(schema.schema, badConfig)).toThrow('Schema configuration error: Invalid default value "text_is_not_okay_here" for item "publication_year".');
-
-    badConfig = deepCopy(config);
-    badConfig['general_parameters/record_information/keywords'] = { defaultValue: 'something' };
-    expect(() => Configuration.configure(schema.schema, badConfig)).toThrow('Schema configuration error: Setting default values for array items is not allowed but "keywords" is an array.');
 });
